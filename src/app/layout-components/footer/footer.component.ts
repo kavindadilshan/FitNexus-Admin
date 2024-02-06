99329a3f81c6ef34dec00e186468fe440ba45214
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-07 02:25:32 +0530

    added footer component

---
+++ src/app/layout-components/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { ThemeOptions } from "../../theme-options";
+
+@Component({
+  selector: "app-footer",
+  templateUrl: "./footer.component.html"
+})
+export class FooterComponent {
+  constructor(public globals: ThemeOptions) {}
+}
