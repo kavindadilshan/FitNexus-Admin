e6be3ee169cbdd6b39b9722170f9cdcc73a98924
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-08 00:03:23 +0530

    create constant file for local storage and server urls

---
+++ src/app/shared/constant/constant.ts
@@ -0,0 +1,48 @@
+const DEV_BASE_URL = "https://fitzky.ceyentra.lk/v1/";
+const PROD_BASE_URL = "https://api.fitzky.com/v1/";
+const CSV_FILE_DEV = "https://fitzky-admin.ceyentra.lk/sample_csv/users.csv";
+const PUBLIC_USER_CSV_FILE_DEV =
+  "https://fitzky-admin.ceyentra.lk/sample_csv/public_users.csv";
+const CSV_FILE_PROD = "https://admin.fitzky.com/sample_csv/users.csv";
+const PUBLIC_USER_CSV_FILE_PROD =
+  "https://admin.fitzky.com/sample_csv/public_users.csv";
+const ACCESS_TOKEN = "access_token";
+const REFRESH_TOKEN = "refresh_token";
+const USER_NAME = "userName";
+const ADMIN_NAME = "adminName";
+const USER_ROLE = "user_role";
+const FIRST_NAME = "first_name";
+const LAST_NAME = "last_name";
+const EMAIL = "email";
+const MOBILE = "mobile";
+const CONDITION = "condition";
+const IMAGE = "image";
+const STUDIO_NAME = "studio_name";
+const TIME_ZONE = "timeZone";
+const VERIFICATION_TYPE = "verificationType";
+const VERIFICATION_NO = "verificationNo";
+const BASIC_TOKEN = "Basic YWRtaW46";
+const CURRENCY = "LKR";
+
+export const constants = {
+  base_url: PROD_BASE_URL,
+  image: IMAGE,
+  user_role: USER_ROLE,
+  access_token: ACCESS_TOKEN,
+  refresh_token: REFRESH_TOKEN,
+  user_name: USER_NAME,
+  admin_name: ADMIN_NAME,
+  basic_token: BASIC_TOKEN,
+  first_name: FIRST_NAME,
+  last_name: LAST_NAME,
+  email: EMAIL,
+  mobile: MOBILE,
+  timeZone: TIME_ZONE,
+  verificationNo: VERIFICATION_NO,
+  verificationType: VERIFICATION_TYPE,
+  condition: CONDITION,
+  studio_name: STUDIO_NAME,
+  currency: CURRENCY,
+  csv_file: CSV_FILE_PROD,
+  public_user_csv_file: PUBLIC_USER_CSV_FILE_PROD
+};
