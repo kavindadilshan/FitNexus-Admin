75d4a23abf62e320460def85f9c1fbb987b69481
kavindadilshan <kavindadilshan97@gmail.com>
2024-02-11 03:29:37 +0530

    created fitness class listing ui

---
+++ src/app/pages/classes/physical-classes/physical-classes.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "physical-classes",
+  templateUrl: "./physical-classes.component.html"
+})
+export class physicalClassesComponent {}
